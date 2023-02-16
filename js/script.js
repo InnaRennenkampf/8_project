c1c5610a5064227ff93ec7a653d2a30421312afd
inna_renn <inna.rennenkampf@gmail.com>
2023-02-16 15:13:29 +0200

    get activity by type

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
-let url = "http://www.boredapi.com/api/activity/"
-$.getJSON (url, function (data) {
-    console.log(data.activity)
-    $('#random-activity').append(
-        $('<p>' + data.activity + '</p>')
-    )
-})
+// let url = "http://www.boredapi.com/api/activity/" //рандомная активность
+// $.getJSON (url, function (data) {
+//     console.log(data.activity)
+//     $('#random-activity').append(
+//         $('<p>' + data.activity + '</p>')
+//     )
+// })
+
+
+$('#type-select').on('change', function () {
+    requestActivityByType(this.value);
+});
+
+function requestActivityByType(type) { //получаем активность по типу
+    let url = "http://www.boredapi.com/api/activity?type=" + type.toLowerCase();
+    console.log(url)
+    $.getJSON(url, function (data) {
+        $('#activity').empty();
+        $('#activity').append(
+            $('<p>' + data.activity + '</p>') 
+        )
+    });
+}
